9ee145f5ecd3da4c3453e160877098b872cffc3d
tmd7619 <tmd814@naver.com>
2022-07-14 20:28:09 +0900

    TIL Ex12

--- src/kr/ac/Chapter01/Ex11.java
+++ src/kr/ac/Chapter01/Ex11.java
@@ -16,6 +16,10 @@ public class Ex11 {
      *
      * 출력
      * 첫 줄에 압축된 문자열을 출력한다.
+     *
+     * String.valueOf() - 파라미터가 null이면 문자열 "null"을 만들어서 반환한다.
+     * toString() - 대상 값이 null이면 NPE를 발생시키고 Object에 담긴 값이 String이 아니여도 출력한다.
+     *
      */
 
     public String solution(String str){
